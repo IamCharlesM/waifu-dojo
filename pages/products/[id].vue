ecccc0f53095008ccce8ccf2c48a48a9cef710eb
iamcharlesm <rafroyevans@gmail.com>
2023-07-25 20:43:55 -0500

    Product detail page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,34 +1,23 @@
 <template>
-    <div>
-        <h1>{{ product.title }}</h1>
-        <p>{{ product.description }}</p>
-        <p>{{ product.price }}</p>
-        <img :src="product.image" alt="product image">
-
-    </div>
+  <div>
+    <productDetails :product="product" />
+  </div>
 </template>
 
 <script setup>
-const { id } = useRoute().params
-const uri = `https://fakestoreapi.com/products/${id}`
+const { id } = useRoute().params;
+const uri = `https://fakestoreapi.com/products/${id}`;
 
 // Fetch product from API
-const { data: product } = await useFetch(uri)
-
-
-
+const { data: product } = await useFetch(uri);
 
 definePageMeta({
-    layout: 'products',
-    title: 'Product title',
-    description: 'Product description',
-    image: 'https://nuxtjs.org/nuxt-card.png',
-    url: 'https://nuxtjs.org'
-})
-
-
+  layout: "products",
+  title: product.title,
+  description: product.description,
+  image: product.image,
+  url: "https://nuxtjs.org",
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
