17d34f11188d9c4e81e1be2592f998e005d9eb34
iamcharlesm <rafroyevans@gmail.com>
2023-07-25 19:39:09 -0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Home</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt commodi esse quo, in perferendis eos quidem sed unde sit tenetur assumenda a? Consectetur repudiandae nam qui et cum voluptatibus quam?
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt commodi esse quo, in perferendis eos quidem sed unde sit tenetur assumenda a? Consectetur repudiandae nam qui et cum voluptatibus quam?
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1 class="text-4xl">Product</h1>
+
+        <p class="text-2xl">
+            {Product details for {{ id }}}
+        </p>
+
+        <p class="text-2xl">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id distinctio accusantium, quod quae quos illum expedita exercitationem? Officia, sapiente atque minus obcaecati eum similique maiores hic tempore repudiandae beatae. Commodi!
+        </p>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
